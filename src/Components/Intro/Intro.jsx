bc1324f60670bbd706e8aef3585e499438261e28
aamirk1 <aamir.k7199@gmail.com>
2023-04-19 23:54:02 +0530

    up

---
+++ src/Components/Intro/Intro.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Intro.css'
+const Intro = () => {
+  return (
+    <div className="intro">
+        <div className="i-left">
+            <div className="i-name">
+                <span>Hye I'm</span>
+                <span>Aamir Khan</span>
+                <span>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat vero ea possimus dolore eveniet ex natus autem dolorum et,</span>
+            </div>
+            <button className="button i-button">Hire me</button>
+        </div>
+        <div className="i-right"></div>
+    </div>
+  )
+}
+
+export default Intro
